43688c709cfc06dd39e91695594539a129fbc3a2
Phu Nguyen <pisun2@Phus-MacBook-Pro.local>
2015-07-26 12:22:25 +0700

    Init backend source code

---
+++ SourceCode/Backend/Zpotdrop/app/Http/Controllers/Admin/AdminController.php
@@ -0,0 +1,20 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| ApiController.php
+|--------------------------------------------------------------------------
+| @Author     : John Nguyen
+| @Email      : pisun2@gmail.com
+| @Copyright  : © 2015 LeapZone.
+| @Date       : 7/26/15 - 11:45 AM
+*/
+
+namespace app\Http\Controllers\Admin;
+
+
+use Illuminate\Routing\Controller as BaseController;
+
+class AdminController extends BaseController
+{
+
+}
---
+++ SourceCode/Backend/Zpotdrop/app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,20 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| ApiController.php
+|--------------------------------------------------------------------------
+| @Author     : John Nguyen
+| @Email      : pisun2@gmail.com
+| @Copyright  : © 2015 LeapZone.
+| @Date       : 7/26/15 - 11:46 AM
+*/
+
+namespace app\Http\Controllers\Api;
+
+
+use Illuminate\Routing\Controller as BaseController;
+
+class ApiController extends BaseController
+{
+
+}
