2aa8298573e91fad0e261222c1b2228575b173b6
hoangson1989 <hoangson1989@gmail.com>
2015-08-04 14:49:39 +0700

    implementing Menu

---
+++ SourceCode/iOS/ZpotDrop/ZpotDrop/ViewController/Menus/LeftMenuViewController.m
@@ -0,0 +1,82 @@
+//
+//  LeftMenuViewController.m
+//  ZpotDrop
+//
+//  Created by ME on 8/4/15.
+//  Copyright (c) 2015 zpotdrop. All rights reserved.
+//
+
+#import "LeftMenuViewController.h"
+#import "MenuProfileTableViewCell.h"
+#import "MenuFeatureTableViewCell.h"
+#import "MenuZpotAllTableViewCell.h"
+#import "BaseTableViewCell.h"
+
+@interface LeftMenuViewController ()<UITableViewDataSource,UITableViewDelegate>
+
+@end
+
+@implementation LeftMenuViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor clearColor];
+    self.view.autoresizesSubviews = YES;
+    self.view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    //======================UITableView=========================//
+    _tableView = [[UITableView alloc]initWithFrame:self.view.bounds style:UITableViewStyleGrouped];
+    _tableView.delegate = self;
+    _tableView.dataSource = self;
+    [self.view addSubview:_tableView];
+    
+    [_tableView registerNib:[UINib nibWithNibName:NSStringFromClass([MenuProfileTableViewCell class]) bundle:nil] forCellReuseIdentifier:NSStringFromClass([MenuProfileTableViewCell class])];
+    [_tableView registerNib:[UINib nibWithNibName:NSStringFromClass([MenuFeatureTableViewCell class]) bundle:nil] forCellReuseIdentifier:NSStringFromClass([MenuFeatureTableViewCell class])];
+    [_tableView registerNib:[UINib nibWithNibName:NSStringFromClass([MenuZpotAllTableViewCell class]) bundle:nil] forCellReuseIdentifier:NSStringFromClass([MenuZpotAllTableViewCell class])];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    return 1;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section{
+    return 0.1;
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section{
+    return 0.1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    BaseTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:[self identifierForIndexPath:indexPath] forIndexPath:indexPath];
+    [cell setupCellWithData:nil andOptions:nil];
+    return cell;
+}
+
+
+-(NSString*)identifierForIndexPath:(NSIndexPath*)indexPath{
+    if (indexPath.row == 0) {
+        return NSStringFromClass([MenuProfileTableViewCell class]);
+    }
+    return NSStringFromClass([MenuProfileTableViewCell class]);
+}
+
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (indexPath.row == 0){
+        return [MenuProfileTableViewCell cellHeight];
+    }
+    return 0;
+}
+@end
