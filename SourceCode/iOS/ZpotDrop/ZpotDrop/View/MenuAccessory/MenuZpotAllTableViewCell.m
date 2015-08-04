c0a5760e680c1cba431573bfe69828ecc68500c5
hoangson1989 <hoangson1989@gmail.com>
2015-08-04 11:30:46 +0700

    rearrange folder

---
+++ SourceCode/iOS/ZpotDrop/ZpotDrop/View/MenuAccessory/MenuZpotAllTableViewCell.m
@@ -0,0 +1,22 @@
+//
+//  MenuZpotAllTableViewCell.m
+//  ZpotDrop
+//
+//  Created by Nguyenh on 8/2/15.
+//  Copyright (c) 2015 zpotdrop. All rights reserved.
+//
+
+#import "MenuZpotAllTableViewCell.h"
+
+@implementation MenuZpotAllTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+}
+
+@end
