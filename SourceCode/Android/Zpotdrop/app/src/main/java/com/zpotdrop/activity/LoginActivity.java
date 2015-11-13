68e382c5686b7ff920a22420c8415f8242378c5b
Trần Huy Phúc <phuc.tran@yeahio.com>
2015-11-13 16:38:02 +0700

    - Implement Login UI

--- SourceCode/Android/Zpotdrop/app/src/main/java/com/zpotdrop/activity/LoginActivity.java
+++ SourceCode/Android/Zpotdrop/app/src/main/java/com/zpotdrop/activity/LoginActivity.java
@@ -1,39 +1,103 @@
+/*
+ * Copyright (c) 2015 Zpotdrop. All rights reserved.
+ */
+
 package com.zpotdrop.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.zpotdrop.R;
+import com.zpotdrop.app.ZpotdropApp;
+import com.zpotdrop.utils.DeviceManager;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+    private final int LOGO_WIDTH = 155;
+    private final int LOGO_HEIGHT = 199;
+    private final int TEXT_LOGO_WIDTH = 322;
+    private final int TEXT_LOGO_HEIGHT = 69;
+
+    @Bind(R.id.iv_logo)
+    ImageView ivLogoZD;
+
+    @Bind(R.id.tv_welcome)
+    TextView tvWelcome;
+
+    @Bind(R.id.iv_text_logo)
+    ImageView ivTextLogoZD;
+
+    @Bind(R.id.edt_email)
+    EditText edtEmail;
+
+    @Bind(R.id.edt_password)
+    EditText edtPassword;
+
+    @Bind(R.id.tv_continue)
+    TextView tvContinue;
+
+    @Bind(R.id.tv_or)
+    TextView tvOr;
+
+    @Bind(R.id.tv_login_with_fb)
+    TextView tvLoginWithFacebook;
+
+    @Bind(R.id.tv_not_yet_on_zd)
+    TextView tvNotYetOnZD;
+
+    @Bind(R.id.tv_forgot_password)
+    TextView tvForgotPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
+
+        setupUI();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    /**
+     * Set up UI: text, size, color,...
+     */
+    private void setupUI() {
+        /**
+         * Adjust logo size
+         */
+        int logoWidth = DeviceManager.getScreenWidth(this) / 4;
+        int logoHeight = (int) ((1.0f * logoWidth / LOGO_WIDTH) * LOGO_HEIGHT);
+        ivLogoZD.getLayoutParams().width = logoWidth;
+        ivLogoZD.getLayoutParams().height = logoHeight;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        /**
+         * Set welcome text font
+         */
+        tvWelcome.setTypeface(ZpotdropApp.openSansLight);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        /**
+         * Adjust text logo size
+         */
+        int textLogoWidth = DeviceManager.getScreenWidth(this) / 2;
+        int textLogoHeight = (int) ((1.0f * textLogoWidth / TEXT_LOGO_WIDTH) * TEXT_LOGO_HEIGHT);
+        ivTextLogoZD.getLayoutParams().width = textLogoWidth;
+        ivTextLogoZD.getLayoutParams().height = textLogoHeight;
 
-        return super.onOptionsItemSelected(item);
+        /**
+         * Set fonts for other texts & inputs
+         */
+        edtEmail.setTypeface(ZpotdropApp.openSansLight);
+        edtPassword.setTypeface(ZpotdropApp.openSansLight);
+        tvContinue.setTypeface(ZpotdropApp.openSansBold);
+        tvOr.setTypeface(ZpotdropApp.openSansLight);
+        tvLoginWithFacebook.setTypeface(ZpotdropApp.openSansLight);
+        tvNotYetOnZD.setTypeface(ZpotdropApp.openSansLight);
+        tvForgotPassword.setTypeface(ZpotdropApp.openSansLight);
     }
 }
+
--- SourceCode/Android/Zpotdrop/app/src/main/java/com/zpotdrop/app/ZpotdropApp.java
+++ SourceCode/Android/Zpotdrop/app/src/main/java/com/zpotdrop/app/ZpotdropApp.java
@@ -1,71 +1,77 @@
+/*
+ * Copyright (c) 2015 Zpotdrop. All rights reserved.
+ */
+
 package com.zpotdrop.app;
 
 import android.app.Application;
-
-import com.parse.Parse;
-import com.parse.ParseACL;
-import com.parse.ParseException;
-import com.parse.ParseFacebookUtils;
-import com.parse.ParseInstallation;
-import com.parse.ParsePush;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
-import com.zpotdrop.utils.SmartLog;
+import android.graphics.Typeface;
 
 /**
  * @author phuc.tran
  */
 public class ZpotdropApp extends Application {
-    private static final String APPLICATION_ID = "ppZUHCYYoJNe1V6ZpAdJfOzJ6vL6mWKKll3V8MM4";
-    private static final String CLIENT_KEY = "sBpKO7QEP3jLS73hSoMAB8NyObATl7vlo4e0qLfC";
+    public static Typeface openSansRegular;
+    public static Typeface openSansLight;
+    public static Typeface openSansBold;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        initParse();
+        createFonts();
+    }
+
+    /**
+     * Create fonts from assets
+     */
+    private void createFonts() {
+        ZpotdropApp.openSansRegular = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
+        ZpotdropApp.openSansLight = Typeface.createFromAsset(getAssets(), "OpenSans-Light.ttf");
+        ZpotdropApp.openSansBold = Typeface.createFromAsset(getAssets(), "OpenSans-Bold.ttf");
     }
 
 
-    private void initParse() {
-        //ParseObject.registerSubclass(User.class);
-        //ParseObject.registerSubclass(Comment.class);
-
-
-        // Enable Local Datastore
-        Parse.enableLocalDatastore(this);
-
-        Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
-        ParseFacebookUtils.initialize(getApplicationContext());
-
-        ParsePush.subscribeInBackground("global", new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e == null) {
-                    SmartLog.error(ZpotdropApp.class, "successfully subscribed to the broadcast channel.");
-                } else {
-                    SmartLog.error(ZpotdropApp.class, e.getMessage());
-                }
-            }
-        });
-
-        ParseACL defaultACL = new ParseACL();
-        defaultACL.setPublicReadAccess(true);
-        defaultACL.setPublicWriteAccess(true);
-        ParseACL.setDefaultACL(defaultACL, true);
-
-        ParseInstallation.getCurrentInstallation().saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e == null) {
-                    SmartLog.error(ZpotdropApp.class, "installed");
-                } else {
-                    SmartLog.error(ZpotdropApp.class, e.getMessage());
-                }
-            }
-        });
-
-        ParseUser.enableRevocableSessionInBackground();
-    }
+//    private void initParse() {
+//        //ParseObject.registerSubclass(User.class);
+//        //ParseObject.registerSubclass(Comment.class);
+//
+//
+//        // Enable Local Datastore
+//        Parse.enableLocalDatastore(this);
+//
+//        Parse.initialize(this, APPLICATION_ID, CLIENT_KEY);
+//        ParseFacebookUtils.initialize(getApplicationContext());
+//
+//        ParsePush.subscribeInBackground("global", new SaveCallback() {
+//            @Override
+//            public void done(ParseException e) {
+//                if (e == null) {
+//                    SmartLog.error(ZpotdropApp.class, "successfully subscribed to the broadcast channel.");
+//                } else {
+//                    SmartLog.error(ZpotdropApp.class, e.getMessage());
+//                }
+//            }
+//        });
+//
+//        ParseACL defaultACL = new ParseACL();
+//        defaultACL.setPublicReadAccess(true);
+//        defaultACL.setPublicWriteAccess(true);
+//        ParseACL.setDefaultACL(defaultACL, true);
+//
+//        ParseInstallation.getCurrentInstallation().saveInBackground(new SaveCallback() {
+//            @Override
+//            public void done(ParseException e) {
+//                if (e == null) {
+//                    SmartLog.error(ZpotdropApp.class, "installed");
+//                } else {
+//                    SmartLog.error(ZpotdropApp.class, e.getMessage());
+//                }
+//            }
+//        });
+//
+//        ParseUser.enableRevocableSessionInBackground();
+//    }
 
 }
+
